a087b41c0f47902b4f6ca9134decfc61a9f8ccd5
nissi153 <36612384+nissi153@users.noreply.github.com>
2022-07-05 15:35:07 +0900

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,86 @@
+    $(document).ready(function(){
+      setTimeout(function(){
+          $('img').each(function() {
+            // http://127.0.0.1:5501/img/mf-icon01.png
+            //console.log( this.src );
+            var src = this.src + '';
+            var arrayStr = src.split('/img/');
+            var orgSrc = '/img/'+arrayStr[1];
+            //console.log( orgSrc );
+            this.src = '.' + orgSrc;
+          });
+
+          $('a').each(function() {
+            // http://127.0.0.1:5501/company/company03.html
+            //console.log( this.href );
+            // 127.0.0.1:5501
+            //console.log( window.location.host );
+            // /index.html
+            // /member/join.html
+            //console.log( window.location.pathname );
+            if( this.href == '#' ||
+                this.href.includes('http') ||
+                this.href.includes('cdn')  ){
+              //넘어간다.
+              return;
+            }
+            var href = this.href + '';
+            var arrayStr = href.split(window.location.host);
+            var orgHref = arrayStr[1];
+            //console.log( orgHref );
+            let pathname = window.location.pathname;
+            if( pathname == '#' ||
+                pathname.includes('http')  ){
+              //넘어간다.
+            }
+            else if( pathname.includes('/member') ||
+                pathname.includes('/company') ||
+                pathname.includes('/business') ||
+                pathname.includes('/product')  ||
+                pathname.includes('/community') ||
+                pathname.includes('/customer') ) {
+              this.href = '../' + orgHref;
+            }else{
+              this.href = '.' + orgHref;
+            }
+            //console.log( this.href );
+          });
+
+          $('link').each(function() {
+            // http://127.0.0.1:5501/css/main.css
+            console.log( this.href );
+            // 127.0.0.1:5501
+            console.log( window.location.host );
+            // /index.html
+            // /member/join.html
+            console.log( window.location.pathname );
+            
+            if( this.href == '#' ||
+                this.href.includes('http') ||
+                this.href.includes('cdn')  ){
+              //넘어간다.
+              return;
+            }
+            var href = this.href + '';
+            var arrayStr = href.split(window.location.host);
+            var orgHref = arrayStr[1];
+            console.log( orgHref );
+
+            let pathname = window.location.pathname;
+            if( pathname.includes('/member') ||
+                pathname.includes('/company') ||
+                pathname.includes('/business') ||
+                pathname.includes('/product')  ||
+                pathname.includes('/community') ||
+                pathname.includes('/customer') ) {
+              this.href = '../' + orgHref;
+            }else{
+              this.href = '.' + orgHref;
+            }
+            console.log( this.href );
+          });
+
+        }, 
+        1000
+      )
+    });
